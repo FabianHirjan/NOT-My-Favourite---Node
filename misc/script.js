4df5d135754d78bffdc3ee11a70b4bd9be00fba7
Hirjan Fabian <35564006+FabianHirjan@users.noreply.github.com>
2024-04-15 13:48:04 +0300

    Reversed the misstakes

--- misc/script.js
+++ misc/script.js
@@ -32,15 +32,4 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         loginForm.submit();
     });
-
-    $(function () {
-        $(window).on("scroll", function () {
-            if ($(window).scrollTop() > 50) {
-                $(".header").addClass("active");
-            } else {
-                //remove the background property so it comes transparent again (defined in your css)
-                $(".header").removeClass("active");
-            }
-        });
-    });
 });
