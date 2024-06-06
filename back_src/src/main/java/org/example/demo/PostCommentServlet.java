9d76be36a59fce587bbdf539c143cccaeb803d59
Supalaplicc <tudi098@gmail.com>
2024-06-06 14:07:25 +0300

    test 50

--- back_src/src/main/java/org/example/demo/PostCommentServlet.java
+++ back_src/src/main/java/org/example/demo/PostCommentServlet.java
@@ -11,21 +11,23 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
-@WebServlet(name = "ApproveArticleServlet", value = "/approve-article-servlet")
-public class ApproveArticleServlet extends HttpServlet {
+@WebServlet(name = "PostCommentServlet", value = "/post-comment-servlet")
+public class PostCommentServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String commentText = request.getParameter("comment");
         String articleId = request.getParameter("articleId");
 
         try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement("UPDATE posts SET approved = 1 WHERE id = ?")) {
+             PreparedStatement statement = connection.prepareStatement("INSERT INTO comments (post_id, comment) VALUES (?, ?)")) {
             statement.setInt(1, Integer.parseInt(articleId));
+            statement.setString(2, commentText);
             statement.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        response.sendRedirect("articles.jsp");
+        response.sendRedirect("viewArticle.jsp?articleId=" + articleId);
     }
 }
--- back_src/src/main/java/org/example/demo/ViewArticleServlet.java
+++ back_src/src/main/java/org/example/demo/ViewArticleServlet.java
@@ -11,67 +11,32 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 
 @WebServlet(name = "ViewArticleServlet", value = "/view-article-servlet")
 public class ViewArticleServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String articleId = request.getParameter("id");
-        if (articleId != null && !articleId.isEmpty()) {
-            Article article = getArticleById(articleId);
-            List<String> comments = getCommentsByArticleId(articleId);
+        String articleId = request.getParameter("articleId");
 
-            if (article != null) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM articles WHERE id = ?")) {
+            statement.setInt(1, Integer.parseInt(articleId));
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                Article article = new Article();
+                article.setId(resultSet.getInt("id"));
+                article.setTitle(resultSet.getString("title"));
+                article.setPoster(resultSet.getString("poster"));
+                // set other fields as necessary
+
                 request.setAttribute("article", article);
-                request.setAttribute("articleComments", comments);
-                request.getRequestDispatcher("/viewArticle.jsp").forward(request, response);
-            } else {
-                response.sendRedirect("/error.jsp");  // Redirect to error page if no article found
-            }
-        } else {
-            response.sendRedirect("/error.jsp");  // Redirect to error page if ID is not provided
-        }
-    }
-
-    private Article getArticleById(String articleId) {
-        Article article = null;
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement("SELECT * FROM posts WHERE id = ?")) {
-            statement.setString(1, articleId);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    article = new Article();
-                    article.setId(resultSet.getInt("id"));
-                    article.setTitle(resultSet.getString("title"));
-                    article.setContent(resultSet.getString("content"));
-                    article.setPoster(resultSet.getString("poster"));
-                    article.setStars(resultSet.getInt("stars"));
-                    article.setType(resultSet.getString("type"));
-                    article.setIsApproved(resultSet.getInt("approved"));
-                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return article;
-    }
 
-    private List<String> getCommentsByArticleId(String articleId) {
-        List<String> comments = new ArrayList<>();
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement("SELECT comment FROM comments WHERE post_id = ?")) {
-            statement.setString(1, articleId);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    comments.add(resultSet.getString("comment"));
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return comments;
+        request.getRequestDispatcher("viewArticle.jsp").forward(request, response);
     }
 }
