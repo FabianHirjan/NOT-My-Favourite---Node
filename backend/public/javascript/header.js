6ceda6c4c7ba01df2f83242989400869095c8df3
Hirjan Fabian <35564006+FabianHirjan@users.noreply.github.com>
2024-06-21 13:43:40 +0300

    fixed my account

---
+++ backend/public/javascript/header.js
@@ -0,0 +1,30 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const token = localStorage.getItem("token");
+    const userNav = document.getElementById("user-nav");
+    const authButtons = document.getElementById("auth-buttons");
+    const adminLink = document.getElementById("admin-link");
+
+    if (token) {
+        console.log("Token exists");
+        userNav.style.display = "block";
+        authButtons.style.display = "none";
+
+        // Decode token
+        const decodedToken = jwt_decode(token);
+
+        // Check if user is admin
+        if (decodedToken.is_admin) {
+            adminLink.style.display = "inline";
+        }
+
+        document.getElementById("logout").addEventListener("click", function () {
+            localStorage.removeItem("token");
+            window.location.reload();
+        });
+    } else {
+        console.log("No token found");
+        userNav.style.display = "none";
+        authButtons.style.display = "block";
+    }
+});
